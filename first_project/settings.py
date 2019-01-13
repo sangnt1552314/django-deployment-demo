9905eedf6b52f32103ef274b1772ed3ba833e6cd
sangga3331 <1552314@hcmut.edu.vn>
2019-01-13 13:57:32 +0700

    edit media dir

--- first_project/settings.py
+++ first_project/settings.py
@@ -19,7 +19,6 @@ TEMPLATES_DIR   = os.path.join(BASE_DIR, "templates")
 STATIC_DIR      = os.path.join(BASE_DIR, "static")
 MEDIA_DIR       = os.path.join(BASE_DIR, "media")
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
